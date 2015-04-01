080c6345c689b2d3d29305615024699bfb473b95
Matt Lanterman <Lanterma@gmail.com>
2015-04-01 16:19:09 -0600

    Added Validations to City and Person models
    [#91337218]

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,4 +6,18 @@ class City < ActiveRecord::Base
     message: 'is too low, lowest point in CO is 3315 feet.'
   }
 
+
+  # country = ISO3166::Country.search('US')
+  # state = country.state
+
+STATE_ABBREVIATIONS = ISO3166::Country.find_country_by_name('united states').states.keys
+
+  validates :state, :presence => true,
+                    :length => { :maximum => 2 },
+                    :inclusion => { :in => STATE_ABBREVIATIONS  },
+                    :confirmation => true
+
+  validates :population, numericality: { greater_than: 0 },
+                          :presence => true
+
 end
